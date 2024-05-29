0831b2dadb3bb802bf5b1a6669b9aa8e7dcd1877
afrizaldm <afrizalmahendra212@gmail.com>
2024-05-29 12:05:35 +0700

    "Refactor app.go, build.bat, public/index.html, routes/web.go, server/server.go, and services/user.go to simplify code and remove unnecessary files"

--- build.bat
+++ build.bat
@@ -7,7 +7,14 @@ set EXECUTABLE_NAME=main.exe
 REM Membuat folder build jika belum ada
 if not exist build (
     mkdir build
+) else (
+    REM Hapus folder build jika sudah ada
+    rmdir /s /q build
+    mkdir build
 )
+
+REM Menyalin folder views ke dalam folder build
+xcopy public build /E /I /Y
 
 REM Menyalin folder views ke dalam folder build
 xcopy resources\views build\views /E /I /Y
