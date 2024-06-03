a7dc341ae80de6b0b242ca2f97dcf74dc1d3d459
afrizaldm <afrizalmahendra212@gmail.com>
2024-06-03 09:12:04 +0700

    configuration vite

---
+++ resources/js/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import { App } from "@/pages/App.vue";
+
+const app = createApp(App)
+//   .use(IonicVue)
+  // .use(pinia)
+  // .use(vuetify)
+  // .use(router)
+  
+  app.mount('#app')
