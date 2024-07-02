bd4f6882059cc8326f940aef7df339cb0df10728
afrizaldm <afrizalmahendra212@gmail.com>
2024-07-02 13:14:14 +0700

    "Refactored build command to separate concerns into individual functions, added timer to measure build duration, and modified logger to create subfolders by year and month."

--- cmd/commands/build.go
+++ cmd/commands/build.go
@@ -1,10 +1,12 @@
 package commands
 
 import (
+	"io"
 	"log"
 	"os"
 	"os/exec"
 	"simple-api/config"
+	"time"
 
 	"github.com/urfave/cli/v2"
 )
@@ -13,78 +15,128 @@ var BuildCommand cli.Command = cli.Command{
 	Name:  "build",
 	Usage: "Build the Go application",
 	Action: func(c *cli.Context) error {
-		var appConfig = config.NewAppConfig().Load()
-		var DBConfig = config.NewDatabaseConfig().Load()
-		var cmd *exec.Cmd
 
-		// Hapus folder build jika ada
-		if err := os.RemoveAll("build"); err != nil {
-			log.Fatalf("Failed to remove build directory: %v", err)
-		}
+		log.Println("Building...")
 
-		// Membuat folder build
-		if err := os.Mkdir("build", 0755); err != nil {
+		// Start timer
+		start := time.Now()
+
+		// clear
+		Clean()
+		SetSQlite()
+		SetLogger()
+		SetENV()
+		SetStatic()
+		SetView()
+		Build()
+
+		duration := time.Since(start)
+
+		log.Println("Build Successful In", duration)
+		return nil
+	},
+}
+
+func Clean() {
+	// Hapus folder build jika ada
+	if err := os.RemoveAll("build"); err != nil {
+		log.Fatalf("Failed to remove build directory: %v", err)
+	}
+
+	// Membuat folder build
+	if err := os.Mkdir("build", 0755); err != nil {
+		log.Fatalf("Failed to create build directory: %v", err)
+	}
+
+	// Hapus folder build\database jika ada
+	if err := os.RemoveAll("build\\database"); err != nil {
+		log.Fatalf("Failed to remove build directory: %v", err)
+	}
+
+	// Hapus folder build\logs jika ada
+	if err := os.RemoveAll("build\\logs"); err != nil {
+		log.Fatalf("Failed to remove build directory: %v", err)
+	}
+}
+
+func SetENV() {
+	// Buka file asli
+	srcFile, err := os.Open(".\\.env")
+	if err != nil {
+		log.Fatalf("Gagal membuka file asli: %v", err)
+	}
+	defer srcFile.Close()
+
+	// Buat file tujuan
+	dstFile, err := os.Create("build\\.env")
+	if err != nil {
+		log.Fatalf("Gagal membuat file tujuan: %v", err)
+	}
+	defer dstFile.Close()
+
+	// Salin konten dari file asli ke file tujuan
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		log.Fatalf("Gagal menyalin file: %v", err)
+	}
+}
+
+func SetStatic() {
+	cmd := exec.Command("xcopy", "public", "build\\public", "/E", "/I", "/Y")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("Failed to copy public directory: %v", err)
+	}
+}
+
+func SetView() {
+	cmd := exec.Command("xcopy", "resources\\views", "build\\views", "/E", "/I", "/Y")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("Failed to copy views directory: %v", err)
+	}
+}
+
+func SetSQlite() {
+	var DBConfig = config.NewDatabaseConfig().Load()
+
+	if DBConfig.DB_CONNECTION == "sqlite" {
+		// Membuat folder build\database
+		if err := os.Mkdir("build\\database", 0755); err != nil {
 			log.Fatalf("Failed to create build directory: %v", err)
 		}
 
-		// Hapus folder build\database jika ada
-		if err := os.RemoveAll("build\\database"); err != nil {
-			log.Fatalf("Failed to remove build directory: %v", err)
-		}
-
-		// Hapus folder build\logs jika ada
-		if err := os.RemoveAll("build\\logs"); err != nil {
-			log.Fatalf("Failed to remove build directory: %v", err)
-		}
-
-		if DBConfig.DB_CONNECTION == "sqlite" {
-			// Membuat folder build\database
-			if err := os.Mkdir("build\\database", 0755); err != nil {
-				log.Fatalf("Failed to create build directory: %v", err)
-			}
-
-			// Menyalin basis data dari database\database.sqlite ke build\database.sqlite
-			cmd = exec.Command("xcopy", "database\\database.sqlite", "build\\database", "/Y")
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			if err := cmd.Run(); err != nil {
-				log.Fatalf("Gagal menyalin basis data: %v", err)
-			}
-		}
-
-		if appConfig.APP_ACTIVE_LOGGING {
-			// Membuat folder build\logs
-			if err := os.Mkdir("build\\logs", 0755); err != nil {
-				log.Fatalf("Failed to create build directory: %v", err)
-			}
-		}
-
-		// Menyalin folder public
-		cmd = exec.Command("xcopy", "public", "build\\public", "/E", "/I", "/Y")
+		// Menyalin basis data dari database\database.sqlite ke build\database.sqlite
+		cmd := exec.Command("xcopy", "database\\database.sqlite", "build\\database", "/Y")
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 		if err := cmd.Run(); err != nil {
-			log.Fatalf("Failed to copy public directory: %v", err)
+			log.Fatalf("Gagal menyalin basis data: %v", err)
 		}
+	}
+}
 
-		// Menyalin folder views
-		cmd = exec.Command("xcopy", "resources\\views", "build\\views", "/E", "/I", "/Y")
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		if err := cmd.Run(); err != nil {
-			log.Fatalf("Failed to copy views directory: %v", err)
+func SetLogger() {
+	var appConfig = config.NewAppConfig().Load()
+
+	if appConfig.APP_ACTIVE_LOGGING {
+		// Membuat folder build\logs
+		if err := os.Mkdir("build\\logs", 0755); err != nil {
+			log.Fatalf("Failed to create build directory: %v", err)
 		}
+	}
+}
 
-		// Membuat executable
-		var filename string = "build\\" + appConfig.APP_NAME + ".exe"
-		cmd = exec.Command("go", "build", "-o", filename, "./cmd/main.go")
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		if err := cmd.Run(); err != nil {
-			log.Fatalf("Failed to build application: %v", err)
-		}
+func Build() {
+	var appConfig = config.NewAppConfig().Load()
 
-		log.Println("Build successful")
-		return nil
-	},
+	var filename string = "build\\" + appConfig.APP_NAME + ".exe"
+	cmd := exec.Command("go", "build", "-o", filename, "./cmd/main.go")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("Failed to build application: %v", err)
+	}
 }
--- cmd/logger/logger.go
+++ cmd/logger/logger.go
@@ -7,10 +7,9 @@ import (
 	"time"
 )
 
-func EnsureLogsFolder() error {
-	// Nama folder
-	folderName := "logs"
+var logRoot string = "logs"
 
+func EnsureLogsFolder(folderName string) error {
 	// Cek apakah folder sudah ada
 	_, err := os.Stat(folderName)
 	if os.IsNotExist(err) {
@@ -35,20 +34,24 @@ func Write(description string) error {
 		return nil
 	}
 
-	EnsureLogsFolder()
+	EnsureLogsFolder(logRoot)
 
 	// Dapatkan tanggal hari ini
 	currentTime := time.Now()
 
-	// Format untuk waktu: "2006-01-02 15:04:05.000"
+	// Nama sub folder dengan kombinasi tahun_bulan
 	logTime := currentTime.Format("2006-01-02 15:04:05.000")
+
+	// Format untuk waktu: "2006-01-02 15:04:05.000"
+	folder_year_month := currentTime.Format("2006-01")
+	EnsureLogsFolder(fmt.Sprintf("logs/%s", folder_year_month))
 
 	// Format baris log: "tanggal jam detik dan mili detik type description"
 	logEntry := fmt.Sprintf("%s: %s\n", logTime, description)
 
 	// Nama file dengan format DD.txt (misal: 01.txt)
 	name := currentTime.Format("02") // Format tanggal sebagai "01" hingga "31"
-	fileName := fmt.Sprintf("logs/%s.txt", name)
+	fileName := fmt.Sprintf("logs/%s/%s.txt", folder_year_month, name)
 
 	// Coba buka atau buat file baru
 	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
