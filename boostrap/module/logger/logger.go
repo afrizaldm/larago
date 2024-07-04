691d2d1f442f351916e14ed9011ef5d8c7c03ca1
afrizaldm <afrizalmahendra212@gmail.com>
2024-07-04 14:21:41 +0700

    "Updated .air.toml to set rerun to true, and modified logger.go to use os.MkdirAll instead of os.Mkdir, and updated log file paths to use logRoot variable."

--- boostrap/module/logger/logger.go
+++ boostrap/module/logger/logger.go
@@ -14,7 +14,7 @@ func EnsureLogsFolder(folderName string) error {
 	_, err := os.Stat(folderName)
 	if os.IsNotExist(err) {
 		// Jika folder tidak ada, buat folder baru
-		err := os.Mkdir(folderName, 0755) // 0755 adalah mode permission untuk folder baru
+		err := os.MkdirAll(folderName, 0755) // 0755 adalah mode permission untuk folder baru
 		if err != nil {
 			return err
 		}
@@ -44,14 +44,14 @@ func Write(description string) error {
 
 	// Format untuk waktu: "2006-01-02 15:04:05.000"
 	folder_year_month := currentTime.Format("2006-01")
-	EnsureLogsFolder(fmt.Sprintf("logs/%s", folder_year_month))
+	EnsureLogsFolder(fmt.Sprintf("%s/%s", logRoot, folder_year_month))
 
 	// Format baris log: "tanggal jam detik dan mili detik type description"
 	logEntry := fmt.Sprintf("%s: %s\n", logTime, description)
 
 	// Nama file dengan format DD.txt (misal: 01.txt)
 	name := currentTime.Format("02") // Format tanggal sebagai "01" hingga "31"
-	fileName := fmt.Sprintf("logs/%s/%s.txt", folder_year_month, name)
+	fileName := fmt.Sprintf("%s/%s/%s.txt", logRoot, folder_year_month, name)
 
 	// Coba buka atau buat file baru
 	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
